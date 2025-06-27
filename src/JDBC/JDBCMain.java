cadd838837ec488ad3f8f7b34c2cef433e784c07
tray0019 <tray0019@algonquinlive.com>
2025-06-27 14:52:33 -0400

    Connected java class to database. Output

--- src/JDBC/JDBCMain.java
+++ src/JDBC/JDBCMain.java
@@ -5,10 +5,54 @@
  */
 package JDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/*
+ * Purpose of this class is to connect
+ * java to sql using JDBC
+ */
 public class JDBCMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String address = "jdbc:mysql://localhost:3306/detectives";
+		String user = "tray0019";
+		String pass = "2025";
+		String script = "select * from detective";
+		
+		/*
+		 * Method for Database Connectivity
+		 */
+		try(Connection con = DriverManager.getConnection(address,user,pass);
+		PreparedStatement statement = con.prepareStatement(script);
+		ResultSet result = statement.executeQuery()){
+			
+			/*
+			 * Print table columns
+			 */
+			System.out.printf("%-15s%-15s%-15s%n",
+					("Detective ID"),
+					("FirstName"),
+					("LastName"));
+			
+			/*
+			 * Print results
+			 */
+			while(result.next()) {
+				System.out.printf("%-15s%-15s%-15s%n",
+						result.getInt("detectiveID"),
+						result.getString("detectiveFNAME"),
+						result.getString("detectiveLNAME"));
+			}
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
 
 	}
 
---
+++ src/JDBC/testMain.java
@@ -0,0 +1,10 @@
+package JDBC;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+	}
+
+}
